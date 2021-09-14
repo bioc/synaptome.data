f05193163358f6b005e8fcbac429da8562fce05d
Anatoly Anatoly <lptolik@gmail.com>
2021-09-14 17:35:43 +0300

    Documentation for the package

---
+++ R/synaptome_data.R
@@ -0,0 +1,18 @@
+#' synaptome.data: AnnotationHub version of Synaptome.DB
+#'
+#' The package provides access to the public copy of the Synaptic
+#' proteome database. It was designed as an accompaniment for
+#' \code{\link{synaptome.db}} package. Database provides information for
+#' specific genes and allows building the protein-protein interaction graph
+#' for gene sets, synaptic compartments, and brain regions. The database is
+#' not intended for direct access. All querying and analysis is supposed to
+#' be performed via \code{\link{synaptome.db}} functionality.
+#'
+#' @docType package
+#' @name synaptome.data
+#'
+#' @references
+#' Sorokina, O., Mclean, C., Croning, M.D.R. et al.  A unified resource and
+#' configurable model of the synapse proteome and its role in disease.
+#' Sci Rep 11, 9967 (2021).  https://doi.org/10.1038/s41598-021-88945-7
+"_PACKAGE"
